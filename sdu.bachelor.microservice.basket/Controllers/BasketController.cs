1fc015c88483446f773492436c73544d468d279c
mnhnielsen <mnhnielsen@gmail.com>
2023-02-14 12:53:43 +0100

    basket-service event pub tests

---
+++ sdu.bachelor.microservice.basket/Controllers/BasketController.cs
@@ -0,0 +1,37 @@
+﻿namespace sdu.bachelor.microservice.basket.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        public const string PubSubName = "kafka-commonpubsub";
+        private readonly ILogger<BasketController> _logger;
+
+        public BasketController(ILogger<BasketController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpPost("checkout")]
+        public async Task<ActionResult<Guid>> CheckoutOrder([FromServices] DaprClient daprClient)
+        {   
+            //Test guid 
+            var basketOrderID = Guid.NewGuid();
+
+            CancellationTokenSource source = new CancellationTokenSource();
+            CancellationToken cancellationTokencancellationToken = source.Token;
+
+            //public event to topic
+            await daprClient.PublishEventAsync(PubSubName,Topics.On_Basket_Checkout, basketOrderID, cancellationTokencancellationToken);
+
+            Console.WriteLine("Checkout Event published with ID: " + basketOrderID);
+            return basketOrderID;
+
+        }
+
+
+        //Add To Basket (Save state in redis cache)
+
+        //Modify Basket (Update state in redis cache)
+    }
+}
---
+++ sdu.bachelor.microservice.common/Topics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sdu.bachelor.microservice.common
+{
+    public class Topics
+    {
+        public const string On_Basket_Checkout = "On_Basket_Checkout";
+    }
+}
