48ddfa98b6695163b88423fdc481e32e2afff461
mnhnielsen <mnhnielsen@gmail.com>
2023-05-29 08:58:33 +0200

    Added requests, abstract

--- sdu.bachelor.microservice.catalog/Data/Catalog.sql
+++ sdu.bachelor.microservice.catalog/Data/Catalog.sql
@@ -20,7 +20,7 @@ CREATE TABLE Products(
     FOREIGN KEY (BrandId) REFERENCES Brands(Id)
 );
 
-INSERT INTO Products VALUES("7201fd50-25b9-4b7d-99a7-b367b73222f8", "cd3eb3f1-0143-495b-9b90-9d1e8e46fbad", "High-End aero bike for the flats", 10000.0, 2147483647, "Madone");
-INSERT INTO Products VALUES("ab0f5a1f-9b48-4862-8e6a-bced8d20558e", "e29de237-8203-4e3e-8066-4ac71d2c707f", "For the mountains", 11000.0, 2147483647, "Vam");
-INSERT INTO Products VALUES("d4b1d999-862d-4cf9-bcb7-b79de08768b9", "e57ed7c0-4cc5-4d12-a88b-ed9f2997d918", "Made for winning", 12000.0, 2147483647, "V4Rs");
+INSERT INTO Products VALUES("7201fd50-25b9-4b7d-99a7-b367b73222f8", "cd3eb3f1-0143-495b-9b90-9d1e8e46fbad", "High-End aero bike for the flats", 10000.0, 100, "Madone");
+INSERT INTO Products VALUES("ab0f5a1f-9b48-4862-8e6a-bced8d20558e", "e29de237-8203-4e3e-8066-4ac71d2c707f", "For the mountains", 11000.0, 100, "Vam");
+INSERT INTO Products VALUES("d4b1d999-862d-4cf9-bcb7-b79de08768b9", "e57ed7c0-4cc5-4d12-a88b-ed9f2997d918", "Made for winning", 12000.0, 100, "V4Rs");
 
